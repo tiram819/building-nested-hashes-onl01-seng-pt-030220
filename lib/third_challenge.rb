a654b5b61e0d020876967572e4dabc44e1fff703
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 07:27:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,11 +6,13 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    }, 
- :patriarch => {name: "Lord Capulet", age: "50"},
+ :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
+}
 
   
     
