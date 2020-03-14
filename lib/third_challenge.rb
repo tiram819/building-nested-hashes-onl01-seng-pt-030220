61c1ffcf9cb4271e594825f13125f3c8f30a1b26
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 06:46:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,7 +26,10 @@ def third_challenge
       :heroine => {
         name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        
+        ]
    }
   }
 
