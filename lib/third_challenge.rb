9140e7b9f1189b58a9421423b67e60d53a669673
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 06:45:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,6 +12,8 @@ def third_challenge
         },
       :hero_friends => [
         {name:"Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        
         ]
    }, 
    :capulet => {
